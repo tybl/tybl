65d607885b99b3d66559e95d54a819db84bf1727
Timothy Lyons <t.brandon.lyons@gmail.com>
2021-07-31 16:01:04 +0000

    Pattern lynel::vector after std::array
    Composing lynel::vector from std::array required a lot of the code to be
    rewritten. So it made sense to me that I should just write the raw
    version instead of trying to compose it.

--- libs/libmath/include/math/lynel/vector.hpp
+++ libs/libmath/include/math/lynel/vector.hpp
@@ -1,24 +1,73 @@
 // License: The Unlicense (https://unlicense.org)
 #pragma once
 
-#include <array>
+#include <cassert>     // assert
+#include <stdexcept>   // std::out_of_range
+#include <type_traits> // std::is_nothrow_swappable
 
 namespace tybl::math::lynel {
 
 template <typename T, std::size_t N>
-class vector {
-  std::array<T,N> m_array;
-public:
+struct vector {
+  static_assert(0 < N, "");
+
+  // types:
   using value_type = T;
+  using reference = value_type&;
+  using const_reference = value_type const&;
+  using iterator = value_type*;
+  using const_iterator = value_type const*;
+  using pointer = value_type*;
+  using const_pointer = value_type const*;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
 
-  [[nodiscard]]
-  constexpr auto operator()(std::size_t i) const -> value_type const& {
-    return m_array[i];
+  value_type m_elems[N];
+
+  // No explicit construct/copy/destroy for aggregate type
+
+  constexpr void fill(const_reference val) {
+    for (auto& elem : m_elems) {
+      elem = val;
+    }
   }
 
-  constexpr auto operator()(std::size_t i) -> value_type& {
-    return m_array[i];
+  constexpr void swap(vector& a) noexcept(std::is_nothrow_swappable<value_type>::value);
+
+  // iterators:
+  [[nodiscard]] constexpr auto begin() noexcept -> iterator { return iterator(data()); }
+  [[nodiscard]] constexpr auto begin() const noexcept -> const_iterator { return const_iterator(data()); }
+  [[nodiscard]] constexpr auto end() noexcept -> iterator { return iterator(data() + N); }
+  [[nodiscard]] constexpr auto end() const noexcept -> const_iterator { return const_iterator(data() + N); }
+
+  // capacity:
+  [[nodiscard]] constexpr auto size() const noexcept -> size_type { return N; }
+  [[nodiscard]] constexpr auto empty() const noexcept -> bool { return N == 0; }
+
+  // element access:
+  [[nodiscard]] constexpr auto operator()(size_type n) noexcept -> reference {
+    assert(n < N);
+    return m_elems[n];
   }
+  [[nodiscard]] constexpr auto operator()(size_type n) const noexcept -> const_reference {
+    assert(n < N);
+    return m_elems[n];
+  }
+  [[nodiscard]] constexpr auto at(size_type n) -> reference {
+    if (n >= N) {
+      throw std::out_of_range("vector::at");
+    }
+    return m_elems[n];
+  }
+  [[nodiscard]] constexpr auto at(size_type n) const -> const_reference {
+    if (n >= N) {
+      throw std::out_of_range("vector::at");
+    }
+    return m_elems[n];
+  }
+
+  [[nodiscard]] constexpr auto data() noexcept -> pointer { return static_cast<pointer>(m_elems); }
+  [[nodiscard]] constexpr auto data() const noexcept -> const_pointer { return static_cast<const_pointer>(m_elems); }
 
 }; // class vector
 
---
+++ libs/libmath/src/math/lynel/vector.cpp
@@ -0,0 +1,10 @@
+#include "math/lynel/vector.hpp"
+
+#include <algorithm>
+
+namespace tybl::math::lynel {
+
+template <typename T, size_t N>
+constexpr void vector<T,N>::swap(vector<T,N>& a) noexcept(std::is_nothrow_swappable<value_type>::value) { std::swap_ranges(data(), data() + N, a.data()); }
+
+} // namespace tybl::math::lynel
