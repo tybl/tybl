0d3bf63eb07727cc7f2141f97ec4ea7eac54e2ae
Timothy Lyons <t.brandon.lyons@gmail.com>
2021-08-30 20:32:16 +0000

    Fix positional assignment of values

--- libs/lynel/include/lynel/matrix.hpp
+++ libs/lynel/include/lynel/matrix.hpp
@@ -3,23 +3,29 @@
 #ifndef TYBL_LYNEL_MATRIX_HPP
 #define TYBL_LYNEL_MATRIX_HPP
 
-#include <array>
+#include <stddef.h> // size_t
 
 namespace tybl::lynel {
 
-template <typename Type, std::size_t Rows, std::size_t Cols>
-class matrix {
-  std::array<Type, Rows * Cols> m_array;
-public:
+template <typename Type, size_t Rows, size_t Cols>
+struct matrix {
+  static_assert(0 < Rows);
+  static_assert(0 < Cols);
+
   using value_type = Type;
+  using reference = value_type&;
+  using const_reference = value_type const&;
+  using size_type = size_t;
+
+  Type m_array[Rows][Cols];
 
   [[nodiscard]]
-  constexpr auto operator()(std::size_t i, std::size_t j) const -> value_type const& {
-    return m_array[i * j];
+  constexpr auto operator()(size_t i, size_t j) const -> const_reference {
+    return m_array[i][j];
   }
 
-  constexpr auto operator()(std::size_t i, std::size_t j) -> value_type& {
-    return m_array[i * j];
+  constexpr auto operator()(size_t i, size_t j) -> reference {
+    return m_array[i][j];
   }
 
 }; // class matrix
--- libs/lynel/test/src/lynel/matrix.cpp
+++ libs/lynel/test/src/lynel/matrix.cpp
@@ -3,8 +3,27 @@
 
 #include <doctest/doctest.h>
 
+using namespace tybl::lynel;
+
 TEST_CASE("tybl::lynel::matrix") {
-  tybl::lynel::matrix<double,3,3> m;
+  matrix<double,3,3> m;
   m(1,1) = 5.5;
   CHECK(m(1,1) == 5.5);
+}
+
+TEST_CASE("aggregate initialization and correct positional assignment of values") {
+  matrix<double,3,3> m = {
+    0,1,2,
+    3,4,5,
+    6,7,8
+  };
+  CHECK(m(0,0) == 0.0);
+  CHECK(m(0,1) == 1.0);
+  CHECK(m(0,2) == 2.0);
+  CHECK(m(1,0) == 3.0);
+  CHECK(m(1,1) == 4.0);
+  CHECK(m(1,2) == 5.0);
+  CHECK(m(2,0) == 6.0);
+  CHECK(m(2,1) == 7.0);
+  CHECK(m(2,2) == 8.0);
 }
