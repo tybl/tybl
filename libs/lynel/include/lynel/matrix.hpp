de916b5ba47eca81c5ddc9360d4277ab8b334447
Timothy Lyons <t.brandon.lyons@gmail.com>
2021-08-30 22:09:55 +0000

    Matrix transpose

--- libs/lynel/include/lynel/matrix.hpp
+++ libs/lynel/include/lynel/matrix.hpp
@@ -3,6 +3,7 @@
 #ifndef TYBL_LYNEL_MATRIX_HPP
 #define TYBL_LYNEL_MATRIX_HPP
 
+#include <assert.h> // assert()
 #include <stddef.h> // size_t
 
 namespace tybl::lynel {
@@ -17,18 +18,37 @@ struct matrix {
   using const_reference = value_type const&;
   using size_type = size_t;
 
-  Type m_array[Rows][Cols];
+  //Type m_array[Rows][Cols];
+  Type m_array[Rows * Cols];
 
   [[nodiscard]]
   constexpr auto operator()(size_t i, size_t j) const -> const_reference {
-    return m_array[i][j];
+    return m_array[i * Cols + j];
   }
 
   constexpr auto operator()(size_t i, size_t j) -> reference {
-    return m_array[i][j];
+    assert(i < Rows);
+    assert(j < Cols);
+    return m_array[i * Cols + j];
+  }
+
+  auto operator*=(value_type s) -> matrix& {
+    for (auto& e : m_array) { e *= s; }
+    return *this;
   }
 
 }; // class matrix
+
+template <typename Type, size_t Rows, size_t Cols>
+auto transpose(matrix<Type,Rows,Cols> const& m) -> matrix<Type,Cols,Rows> {
+  matrix<Type,Cols,Rows> result;
+  for (size_t i = 0; i < Rows; ++i) {
+    for (size_t j = 0; j < Cols; ++j) {
+      result(j,i) = m(i,j);
+    }
+  }
+  return result;
+}
 
 } // namespace tybl::lynel
 
--- libs/lynel/test/src/lynel/matrix.cpp
+++ libs/lynel/test/src/lynel/matrix.cpp
@@ -27,3 +27,39 @@ TEST_CASE("aggregate initialization and correct positional assignment of values"
   CHECK(m(2,1) == 7.0);
   CHECK(m(2,2) == 8.0);
 }
+
+TEST_CASE("scalar multiplication") {
+  matrix<double,3,3> m = {
+    0,1,2,
+    3,4,5,
+    6,7,8
+  };
+  m *= 2.0;
+  CHECK(m(0,0) == 0.0);
+  CHECK(m(0,1) == 2.0);
+  CHECK(m(0,2) == 4.0);
+  CHECK(m(1,0) == 6.0);
+  CHECK(m(1,1) == 8.0);
+  CHECK(m(1,2) == 10.0);
+  CHECK(m(2,0) == 12.0);
+  CHECK(m(2,1) == 14.0);
+  CHECK(m(2,2) == 16.0);
+}
+
+TEST_CASE("transpose 3x3") {
+  matrix<double,3,3> m = {
+    0,1,2,
+    3,4,5,
+    6,7,8
+  };
+  auto n = transpose(m);
+  CHECK(n(0,0) == m(0,0));
+  CHECK(n(1,0) == m(0,1));
+  CHECK(n(2,0) == m(0,2));
+  CHECK(n(0,1) == m(1,0));
+  CHECK(n(1,1) == m(1,1));
+  CHECK(n(2,1) == m(1,2));
+  CHECK(n(0,2) == m(2,0));
+  CHECK(n(1,2) == m(2,1));
+  CHECK(n(2,2) == m(2,2));
+}
