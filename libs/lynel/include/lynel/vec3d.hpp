c38c9c6ed3a8456d7e57aabad1e58475d1c0b112
Timothy Lyons <t.brandon.lyons@gmail.com>
2021-09-02 19:35:05 +0000

    Initial vec3d struct
    I created a simple struct with three double precision floating point
    numbers representing x, y, and z. These can be accessed directly,
    without accessors. There is no constructor so it will require aggregate
    initialization for now. This seems sufficient as a starting point until
    I go further in replacing Eigen's Vector3d in libcoord.
    
    Relevant links:
    - Issue 3: Use my own types in libcoord instead of relying on Eigen
    (https://github.com/tybl/tybl/issues/3)

---
+++ libs/lynel/include/lynel/vec3d.hpp
@@ -0,0 +1,16 @@
+// License: The Unlicense (https://unlicense.org)
+#pragma once
+#ifndef TYBL_LYNEL_VEC3D_HPP
+#define TYBL_LYNEL_VEC3D_HPP
+
+namespace tybl::lynel {
+
+struct vec3d {
+  double x;
+  double y;
+  double z;
+}; // struct vec3d
+
+} // namespace tybl::lynel
+
+#endif // TYBL_LYNEL_VEC3D_HPP
