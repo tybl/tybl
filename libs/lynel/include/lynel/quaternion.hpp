105bbe1b541345c2e06552275b542b09777d6fb1
Timothy Lyons <t.brandon.lyons@gmail.com>
2021-09-04 00:44:51 +0000

    Initial quaternion struct
    I created a simple struct with four double precision floating point
    numbers representing the scalar (w) and the vector components (x, y,
    and z). These can be accessed directly, without accessors. There is no
    constructor so it will require aggregate initialization for now.
    
    Relevant links:
    - Issue 3: Use my own types in libcoord instead of relying on Eigen
    (https://github.com/tybl/tybl/issues/3)

---
+++ libs/lynel/include/lynel/quaternion.hpp
@@ -0,0 +1,17 @@
+// License: The Unlicense (https://unlicense.org)
+#pragma once
+#ifndef TYBL_LYNEL_QUATERNION_HPP
+#define TYBL_LYNEL_QUATERNION_HPP
+
+namespace tybl::lynel {
+
+struct quaternion {
+  double x;
+  double y;
+  double z;
+  double w;
+}; // struct quaternion
+
+} // namespace tybl::lynel
+
+#endif // TYBL_LYNEL_QUATERNION_HPP
