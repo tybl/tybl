98a2120bcc08ea6cf31fdba3efeea1f082c7722c
Timothy Lyons <t.brandon.lyons@gmail.com>
2021-10-28 11:51:02 +0000

    wip

--- apps/StackSolver/src/find_all.cpp
+++ apps/StackSolver/src/find_all.cpp
@@ -3,12 +3,51 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <unordered_set>
 #include <vector>
+
+#define SET std::unordered_set
+
+typedef unsigned __int128 uint128_t;
+
+template <typename T>
+class vector_set {
+  std::vector<T> m_data;
+public:
+  vector_set() { m_data.reserve(226800); }
+  size_t size() const { return m_data.size(); }
+  bool contains(T const& t) const {
+    return std::binary_search(m_data.begin(), m_data.end(), t);
+  }
+  void insert(T t) {
+    auto i = std::upper_bound(m_data.begin(), m_data.end(), t);
+    m_data.insert(i,t);
+    if (!std::is_sorted(m_data.begin(), m_data.end())) {
+      std::cout << "Not sorted\n";
+    }
+  }
+#if 0
+  template <typename I>
+  void insert(I beg, I end) {
+    std::sort(beg,end);
+    auto i = m_data.insert(m_data.end(),beg,end);
+    std::merge(m_data.begin(), i, i, m_data.end(), m_data.begin());
+  }
+#endif
+};
 
 class Node {
   std::vector<std::string> m_contents;
   size_t m_height;
 public:
+
+  size_t hex2uint(char c) const {
+    return (c < 'A') ? c - '0' : c - 'A' + 0xA;
+  }
+
+  char uint2hex(size_t i) const {
+    return (i < 9) ? '1' + i : 'A' + (i - 9);
+  }
 
   Node(size_t h, size_t n)
     : m_contents(n + 2)
@@ -16,8 +55,7 @@ public:
   {
     for (size_t i = 0; i < n; ++i) {
       for (size_t j = 0; j < h; ++j) {
-        char c = (i < 9) ? '1' + i : 'A' + (i - 9);
-        m_contents.at(i).push_back(c);
+        m_contents.at(i).push_back(uint2hex(i));
       }
     }
   }
@@ -27,7 +65,7 @@ public:
     , m_height(o.m_height)
   {}
 
-  auto get_next(std::set<Node> const& found) const -> std::vector<Node> {
+  auto get_next(SET<Node>& found) const -> std::vector<Node> {
     std::vector<Node> result;
     for (uint8_t i = 0; i < static_cast<uint8_t>(m_contents.size()); ++i) {
       for (uint8_t j = 0; j < static_cast<uint8_t>(m_contents.size()); ++j) {
@@ -35,6 +73,7 @@ public:
           Node newb(*this);
           newb.apply(i,j);
           if (!found.contains(newb)) {
+            found.insert(newb);
             result.push_back(newb);
           }
         }
@@ -47,10 +86,6 @@ public:
     return m_contents < o.m_contents;
   }
 
-  size_t hex2uint(char c) const {
-    return (c < 'A') ? c - '0' : c - 'A' + 0xA;
-  }
-
   size_t encode() const {
     std::cerr << "Encoding " << m_contents.size() << " stacks of height " << m_height << "\n";
     std::set<char> colors;
@@ -60,29 +95,30 @@ public:
       }
     }
     std::cerr << colors.size() << " colors\n";
-    size_t result = 0;
+    uint128_t result = 0;
     for (auto const& s : m_contents) {
       size_t digit = 0;
       for (auto const& c : s) {
-        std::cerr << c << " " << std::hex << hex2uint(c) << '\n';
         digit *= colors.size();
         digit += c;
       }
+      uint128_t prev = result;
       result *= m_height * colors.size();
       result += digit;
+      if (prev > result) std::cerr << "overflow\n";
     }
     return result;
   }
 
-  void print() const {
+  void print(std::ostream& o) const {
     for (size_t i = 0; i < m_height; ++i) {
-      std::cout << '\n';
+      o << '\n';
       size_t r = (m_height - 1) - i;
       for (auto const& c : m_contents) {
         if (r < c.size()) {
-          std::cout << c.at(r);
+          o << c.at(r);
         } else {
-          std::cout << ' ';
+          o << ' ';
         }
       }
     }
@@ -101,28 +137,26 @@ private:
 }; // class Node
 
 size_t count_nodes(size_t height, size_t num_unique) {
-  std::set<Node> found;
+  SET<Node> found(226800);
   std::vector<Node> queue { Node(height, num_unique) };
   found.insert(queue.back());
-  queue.back().print();
-  queue.back().encode();
-  /*
+  //queue.back().print();
+  //queue.back().encode();
   do {
     Node curr = queue.back();
     queue.pop_back();
     auto next = curr.get_next(found);
-    found.insert(next.begin(), next.end());
+    //found.insert(next.begin(), next.end());
     queue.insert(queue.end(), next.begin(), next.end());
   } while (queue.size());
-  */
   return found.size();
 }
 
 auto main() -> int {
-  for (size_t h = 1; h < 4; ++h) {
-    for (size_t n = 1; n < 12; ++n) {
+  for (size_t h = 1; h <= 3; ++h) {
+    for (size_t n = 1; n <= 3; ++n) {
       count_nodes(h,n);
-      //std::cout << '(' << h << ',' << n << ") = " << count_nodes(h,n) << '\n';
+      std::cout << '(' << h << ',' << n << ") = " << count_nodes(h,n) << '\n';
     }
   }
 }
