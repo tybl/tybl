b78666b08dd23dce23022401fcad3392a45a252f
Timothy Lyons <t.brandon.lyons@gmail.com>
2022-01-30 13:36:43 +0000

    Consolidate parse_error

--- libs/libvodka/include/vodka/parse_error.hpp
+++ libs/libvodka/include/vodka/parse_error.hpp
@@ -1,18 +1,16 @@
 // License: The Unlicense (https://unlicense.org)
 #pragma once
-#ifndef TYBL_VODKA_PARSEERROR_HPP
-#define TYBL_VODKA_PARSEERROR_HPP
+#ifndef _TYBL__VODKA__PARSE_ERROR__HPP_
+#define _TYBL__VODKA__PARSE_ERROR__HPP_
 
 #include <stdexcept>
 
 namespace tybl::vodka {
 
-struct parse_error
-  : public std::runtime_error
-{
+struct parse_error : public std::runtime_error {
   using std::runtime_error::runtime_error;
 }; // struct parse_error
 
 } // namespace tybl::vodka
 
-#endif // TYBL_VODKA_PARSEERROR_HPP
+#endif // _TYBL__VODKA__PARSE_ERROR__HPP_
