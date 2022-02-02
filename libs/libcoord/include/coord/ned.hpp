5c39347770de5e2dae3f9471bf24eb3f3e9b0ea9
Timothy Lyons <t.brandon.lyons@gmail.com>
2022-02-02 16:42:36 -0500

    More pure functions

--- libs/libcoord/include/coord/ned.hpp
+++ libs/libcoord/include/coord/ned.hpp
@@ -13,19 +13,19 @@ class ned_t {
   lynel::cvec3<double> m_ned;
 
 public:
-  [[nodiscard]] auto to_ecef(ecef_t const& p_ref_pos) const -> ecef_t;
+  [[nodiscard,gnu::pure]] auto to_ecef(ecef_t const& p_ref_pos) const -> ecef_t;
 
-  [[nodiscard]] auto x() const -> double;
+  [[nodiscard,gnu::pure]] auto x() const -> double;
 
-  [[nodiscard]] auto y() const -> double;
+  [[nodiscard,gnu::pure]] auto y() const -> double;
 
-  [[nodiscard]] auto z() const -> double;
+  [[nodiscard,gnu::pure]] auto z() const -> double;
 
-  [[nodiscard]] auto n() const -> double;
+  [[nodiscard,gnu::pure]] auto n() const -> double;
 
-  [[nodiscard]] auto e() const -> double;
+  [[nodiscard,gnu::pure]] auto e() const -> double;
 
-  [[nodiscard]] auto d() const -> double;
+  [[nodiscard,gnu::pure]] auto d() const -> double;
 
 }; // class ned_t
 
