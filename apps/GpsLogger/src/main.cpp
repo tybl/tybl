41905f29797fae9838de79904f83de578b70f88e
Timothy Lyons <t.brandon.lyons@gmail.com>
2022-03-13 17:37:33 -0400

    Use fmtlib for printing

--- apps/GpsLogger/src/main.cpp
+++ apps/GpsLogger/src/main.cpp
@@ -3,11 +3,10 @@
 
 #include <vodka/parse_error.hpp>
 
+#include <fmt/format.h>
 #include <spdlog/spdlog.h>
 
 #include <fstream>
-#include <iomanip>
-#include <iostream>
 
 auto main(int argc, char* argv[]) -> int {
   spdlog::set_level(spdlog::level::err);
@@ -17,25 +16,14 @@ auto main(int argc, char* argv[]) -> int {
     tybl::nmea::byte_parser parser;
     tybl::nmea::gps_service gps(parser);
 
-    std::cout << "fix  Sats  Sig\t\tSpeed    Dir  Lat       , Lon           Accuracy" << std::endl;
+    fmt::print("fix  Sats  Sig\t\tSpeed    Dir  Lat       , Lon           Accuracy\n");
     // Handle any changes to the GPS fix... This is called whenever it's updated.
     gps.on_update += [&gps]() {
-      std::cout << (gps.fix.locked() ? "[*] " : "[ ] ") << std::setw(2) << std::setfill(' ')
-                << gps.fix.tracking_satellites << "/" << std::setw(2) << std::setfill(' ') << gps.fix.visible_satellites
-                << " ";
-      std::cout << std::fixed << std::setprecision(2) << std::setw(5) << std::setfill(' ')
-                << gps.fix.m_almanac.average_snr() << " dB   ";
-      std::cout << std::fixed << std::setprecision(2) << std::setw(6) << std::setfill(' ') << gps.fix.speed << " km/h ["
-                << tybl::nmea::gps::fix::ordinal_direction(gps.fix.travel_angle, true) << "]  ";
-      std::cout << std::fixed << std::setprecision(6) << gps.fix.latitude
-                << " "
-                   "N, "
-                << gps.fix.longitude
-                << " "
-                   "E"
-                << "  ";
-      std::cout << "+/- " << std::setprecision(1) << gps.fix.horizontal_accuracy() << "m  ";
-      std::cout << std::endl;
+      fmt::print("[{}] {: >2}/{: >2} {: >5.2f} dB   {: >6.2f} km/h [{}]  {:.6f} N, {:.6f} E  +/- {:.1f}m  \n",
+                 gps.fix.locked() ? '*' : ' ', gps.fix.tracking_satellites, gps.fix.visible_satellites,
+                 gps.fix.m_almanac.average_snr(), gps.fix.speed,
+                 tybl::nmea::gps::fix::ordinal_direction(gps.fix.travel_angle, true),
+                 gps.fix.latitude, gps.fix.longitude, gps.fix.horizontal_accuracy());
     };
 
     std::string line;
@@ -51,7 +39,6 @@ auto main(int argc, char* argv[]) -> int {
     }
 
     // Show the final fix information
-    std::cout << gps.fix.to_string() << std::endl;
-    std::cin.ignore();
+    fmt::print("{}\n", gps.fix.to_string());
   }
 }
